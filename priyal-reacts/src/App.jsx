ad4915d33b3b0f62fdb11eed0a831966a1418baa
priyal.gupta <priyal.gupta@samarth.ac.in>
2024-01-02 16:58:03 +0530

    first component

---
+++ priyal-reacts/src/App.jsx
@@ -0,0 +1,13 @@
+import FirstComponent from "./FirstComp"
+
+
+function App() {
+  return (
+    <>
+      <h1>Priyal Reacts:</h1>
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ priyal-reacts/src/FirstComp.jsx
@@ -0,0 +1,5 @@
+function FirstComponent(){
+    return (<h3>This is my first component in React</h3>)
+}
+
+export default FirstComponent
