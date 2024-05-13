f978b880cf5b66ed993b36a6247e8738a6cd1bdb
priyal.gupta <priyal.gupta@samarth.ac.in>
2024-05-13 18:34:26 +0530

    test file

--- markdown-viewer/server.js
+++ markdown-viewer/server.js
@@ -1,42 +1,32 @@
 var express = require('express');
-var app = express();
+    var app = express();
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+    // set the view engine to ejs
+    app.set('view engine', 'ejs');
 
-// public folder to store assets
-app.use(express.static(__dirname + '/public'));
+    // public folder to store assets
+    app.use(express.static(__dirname + '/public'));
 
-// routes for app
-app.get('/', function(req, res) {
-    res.render('pad');
-});
-app.get('/(:id)', function(req, res) {
-    res.render('pad');
-});
+    // routes for app
+    app.get('/', function(req, res) {
+      res.render('test');
+    });
+    app.get('/(:id)', function(req, res) {
+      res.render('test');
+    });
 
-// get sharejs dependencies
-var sharejs = require('share');
+    // get sharejs dependencies
+    // var sharejs = require('share');
+    // require('redis');
 
-// set up redis server
-var redisClient;
-console.log(process.env.REDISTOGO_URL);
-if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-    redisClient.auth(rtg.auth.split(":")[1]);
-} else {
-    redisClient = require("redis").createClient();
-}
+    // // options for sharejs
+    // var options = {
+    //   db: {type: 'redis'},
+    // };
 
-// options for sharejs
-var options = {
-    db: {type: 'redis', client: redisClient}
-};
+    // // attach the express server to sharejs
+    // sharejs.server.attach(app, options);
 
-// attach the express server to sharejs
-sharejs.server.attach(app, options);
-
-// listen on port 8000 (for localhost) or the port defined for heroku
-var port = process.env.PORT || 8000;
-app.listen(port);
+    // listen on port 8000 (for localhost) or the port defined for heroku
+    var port = process.env.PORT || 8000;
+    app.listen(port);
